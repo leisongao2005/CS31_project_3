dcfb59b5f55cf82ed3e532c6fd9b43f086270db8
Leison Gao <leisongao2005@g.ucla.edu>
2023-10-22 14:13:49 -0700

    Line functions

---
+++ project_3/grid.cpp
@@ -0,0 +1,8 @@
+//
+//  grid.cpp
+//  project_3
+//
+//  Created by Leison Gao on 10/22/23.
+//
+
+#include "grid.hpp"
---
+++ project_3/grid.h
@@ -0,0 +1,13 @@
+//
+//  grid.hpp
+//  project_3
+//
+//  Created by Leison Gao on 10/22/23.
+//
+
+#ifndef grid_hpp
+#define grid_hpp
+
+#include <stdio.h>
+
+#endif /* grid_hpp */
